f0202b488b9381ff80f683bd4818143988019bdd
Kevin Q Li <likevinq@gmail.com>
2016-10-15 11:26:19 -0500

    Made things nicer!

--- app/app.py
+++ app/app.py
@@ -1,33 +1,34 @@
 from flask import Flask, render_template, flash, request
 from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
- 
+import re
+
 # App config.
 DEBUG = True
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
- 
+
 class ReusableForm(Form):
-    name = TextField('Name:', validators=[validators.required()])
- 
- 
+  name = TextField('Name:', validators=[validators.required()])
+
+
 @app.route("/", methods=['GET', 'POST'])
 def hello():
-    form = ReusableForm(request.form)
- 
-    print form.errors
-    if request.method == 'POST':
-        name=request.form['name']
-        print name
-        embed_url=name.replace("watch?v=", "embed/")
- 
-        if form.validate():
-            # Save the comment here.
-            return '<h1>Hello world!</h1>\n<iframe src="' + embed_url + '" width="853" height="480" frameborder="0" allowfullscreen></iframe>'
-        else:
-            flash('Error: All the form fields are required. ')
- 
+  form = ReusableForm(request.form)
+  print form.errors
+  if request.method == 'POST':
+    url=request.form['name']
+    print url
+    match = re.search(r"watch\?v=(.*)$", url)
+    if match and form.validate():
+      video_id = match.group(1)
+      flash('See results below.')
+      return render_template('hello.html', form=form, video_id=video_id)
+    else:
+      flash('Error: YouTube URL is not valid.')
+      return render_template('hello.html', form=form)
+  else:
     return render_template('hello.html', form=form)
- 
+
 if __name__ == "__main__":
-    app.run()
+  app.run()
