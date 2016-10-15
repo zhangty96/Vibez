a5da09650d876e9e71f0b70fad25768ca814a0bd
TianyiZhang <zhangtianyi96@hotmail.com>
2016-10-15 15:45:48 -0500

    Added test.py

---
+++ app/test.py
@@ -0,0 +1,3 @@
+from alchemy import keyword
+
+print keyword("Our jobs are fleeing the country. They're going to Mexico. They're going to many other countries. You look at what China is doing to our country in terms of making our product. They're devaluing their currency, and there's nobody in our government to fight them. And we have a very good fight. And we have a winning fight. Because they're using our country as a piggy bank to rebuild China, and many other countries are doing the same thing.")
