adfbae8d5e5613e11d783928c8f926d9eb0c0752
TianyiZhang <zhangtianyi96@hotmail.com>
2016-10-15 15:56:17 -0500

    Changed emotion.py to Alchemy.py

--- app/emotion.py
+++
@@ -1,6 +0,0 @@
-import json
-from watson_developer_cloud import AlchemyLanguageV1
-
-def emotion(text_input):
-	alchemy_language = AlchemyLanguageV1(api_key='5081e79ae767e8bdbb26f64def8f9615255a2913')
-	print(json.dumps(alchemy_language.emotion(text=text_input), indent=2))
