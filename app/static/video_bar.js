13638857a7e303cba399fe0c5fcae8ac6a0f8ae6
Zilu Tang <Zilu Tang>
2016-10-15 22:54:22 -0500

    changed draw bar to a function

--- app/static/video_bar.js
+++ app/static/video_bar.js
@@ -1,14 +1,4 @@
 $(function() {
-	var c = document.getElementById("myCanvas");
-	var bar = c.getContext("2d");
-	    bar.rect(0, 0, 600, 20);
-	    bar.stroke();
-	var myarray = [1,2,3,4,5,8,11,20,34,50];
-	for (i = 0; i < myarray.length; i++) {
-	    var ctx = c.getContext("2d");
-	    ctx.rect(myarray[i]*10, 0, 2, 20);
-	    ctx.stroke();
-	}
 	$("#myCanvas").click(function(e){
    		var parentOffset = $(this).offset(); 
    		var relX = e.pageX - parentOffset.left;
@@ -16,3 +6,17 @@ $(function() {
  		seekTo(parseInt(relX/600*player.getDuration()));
 	});
 });
+
+(function( $ ) {
+	$.draw_bar = $.fn.draw_bar = function(inputarray) {
+	var c = document.getElementById("myCanvas");
+	var bar = c.getContext("2d");
+	    bar.rect(0, 0, 600, 20);
+	    bar.stroke();
+	var myarray = inputarray;
+	for (i = 0; i < myarray.length; i++) {
+	    var ctx = c.getContext("2d");
+	    ctx.rect(myarray[i], 0, 2, 20);
+	    ctx.stroke();
+	}};
+})( jQuery );
