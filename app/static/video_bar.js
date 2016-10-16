d8d4db8a0e8b8223df02df0e6dfd6895fbaae2e1
Zilu Tang <Zilu Tang>
2016-10-15 23:32:08 -0500

    made bar white, calibrate the search bars

--- app/static/video_bar.js
+++ app/static/video_bar.js
@@ -1,9 +1,11 @@
+var VIDWIDTH = 600;
+
 $(function() {
 	$("#myCanvas").click(function(e){
    		var parentOffset = $(this).offset(); 
    		var relX = e.pageX - parentOffset.left;
- 		console.log(parseInt(relX/600*player.getDuration()));
- 		seekTo(parseInt(relX/600*player.getDuration()));
+ 		console.log(relX/VIDWIDTH*player.getDuration());
+ 		seekTo(relX/VIDWIDTH*player.getDuration());
 	});
 });
 
@@ -11,14 +13,14 @@ $(function() {
 	$.draw_bar = $.fn.draw_bar = function(inputarray) {
 	var c = document.getElementById("myCanvas");
 	var bar = c.getContext("2d");
-	    bar.fillStyle = "#white";
-	    bar.rect(0, 0, 600, 20);
+	    bar.fillStyle = "white";
+	    bar.rect(0, 0, VIDWIDTH, 20);
 	    bar.stroke();
 	var myarray = inputarray;
 	for (i = 0; i < myarray.length; i++) {
 	    var ctx = c.getContext("2d");
 	    ctx.fillStyle = "white";
-	    ctx.rect(myarray[i], 0, 2, 20);
+	    ctx.rect(myarray[i]/player.getDuration()*VIDWIDTH, 0, 2, 20);
 	    ctx.stroke();
 	}};
 })( jQuery );
