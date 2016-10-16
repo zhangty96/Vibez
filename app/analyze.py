862626ed113551b7b0ff002bb9fb19f264307e6f
Kevin Q Li <likevinq@gmail.com>
2016-10-15 21:49:59 -0500

    Fixed audio file have no words

--- app/analyze.py
+++ app/analyze.py
@@ -14,6 +14,8 @@ from emotion_parser import emotion_parser
 
 def analyze(filename):
   tts_json = tts(filename)
+  tts_json.strip() == '':
+    return ([], [], {}, [], [])
   wordlist, wordseg = script_parser(tts_json)
   words = []
   locs = []
@@ -31,13 +33,14 @@ def analyze(filename):
   emotions = {}
   for start_time, phrase in wordseg:
     phrase = ' '.join(phrase)
-    emotion_json = emotion(phrase)
-    emotion_map = emotion_parser(emotion_json)
-    seg_locs.append(start_time)
-    for emo, score in emotion_map.iteritems():
-      if emo not in emotions:
-        emotions[emo] = []
-      emotions[emo].append(float(score))
+    if phrase.strip() != '':
+      emotion_json = emotion(phrase)
+      emotion_map = emotion_parser(emotion_json)
+      seg_locs.append(start_time)
+      for emo, score in emotion_map.iteritems():
+        if emo not in emotions:
+          emotions[emo] = []
+        emotions[emo].append(float(score))
   emotions = [{"name": emo, "data": scores} for emo, scores in emotions.iteritems()]
   return (words, locs, word_locs, seg_locs, emotions)
 
